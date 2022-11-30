9b4fdf87d5489980839d71db7d0fcb7aba49a21e
XxshiftxX <qjfrntop12@gmail.com>
2022-11-30 20:00:15 +0900

    ActiveRecord 패턴 적용

--- src/applications/conversation.application.ts
+++ src/applications/conversation.application.ts
@@ -1,22 +1,17 @@
-import { ReactionRepository } from "src/repository/reaction/reaction.repository"
-import { UserRepository } from "src/repository/user/user.repository"
+import { Reaction } from "src/domain/reaction/reaction.entity"
+import { User } from "src/domain/user/user.entity"
 import { BusinessError } from "src/utils/business-error"
 
 export class ConversationApplication {
-  private readonly userRepository = new UserRepository()
-  private readonly reactionRepository = new ReactionRepository()
-
   public async converse(userId: string, content: string) {
-    const user = await this.userRepository.findUser(userId)
+    const user = await User.findOne({ id: userId })
     if (!user) return new BusinessError("user-not-found")
 
-    const reaction = await this.reactionRepository.findReactionByKeyword(content)
-    console.log(reaction)
+    const reaction = await Reaction.findReactionByKeyword(content)
     if (!reaction) return new BusinessError("reaction-not-found")
 
     user.likability += reaction.reward
-
-    await this.userRepository.saveUser(user)
+    user.save()
 
     return { user, reaction }
   }
--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,21 +1,44 @@
-export interface IUser {
+import { model, Model, Schema, Types } from "mongoose"
+import { Empty } from "src/utils/empty"
+
+export interface UserDocument extends Document {
   id: string
   username: string
   likability: number
   battery: number
   badges: string[]
   verifiedAt?: Date
+}
+
+export interface UserVirtuals {
   likeLevel: number
 }
 
-export function getLikeLevel(this: IUser): number {
-  const { likability } = this
+const userSchema = new Schema<UserDocument, Model<UserDocument>, Empty, Empty, UserVirtuals>(
+  {
+    id: { type: String, required: true },
+    username: { type: String, required: true },
+    likability: { type: Number, required: true, default: 0 },
+    battery: { type: Number, required: true, default: 100 },
+    badges: { type: [Types.ObjectId], required: true, default: [] },
+  },
+  {
+    virtuals: {
+      likeLevel: {
+        get: function (): number {
+          const { likability } = this
 
-  if (likability >= 1500) return 5
-  if (likability >= 700) return 4
-  if (likability >= 200) return 3
-  if (likability >= 30) return 2
-  if (likability >= -5) return 1
+          if (likability >= 1500) return 5
+          if (likability >= 700) return 4
+          if (likability >= 200) return 3
+          if (likability >= 30) return 2
+          if (likability >= -5) return 1
 
-  return 0
-}
+          return 0
+        },
+      },
+    },
+  }
+)
+
+export const User = model("users", userSchema)
